df041153c9669a4cacef6bcf108b6f73310b8f27
tommycroot <tommycroot@rocketmail.com>
2021-10-10 16:32:05 +0100

    clone

---
+++ src/Router.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Home from "./components/home/Home";
+import Navbar from "./components/misc/Navbar";
+
+
+function Router() {
+  return (
+    <BrowserRouter>
+    <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
